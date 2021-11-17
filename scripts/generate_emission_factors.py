6626a28826f69e2514c70cec79ab79911ed9cfc0
Helen Root <helenroot1@gmail.com>
2021-11-17 10:58:34 +0100

    Include 0 emission factors

--- scripts/generate_emission_factors.py
+++ scripts/generate_emission_factors.py
@@ -11,30 +11,23 @@ def ƒetch_emissions():
 
     if body["total"] > len(results):
         raise Exception(
-            f"There {body['total']} emissions but only {len(results)} fetched, please revise script/query to get all results"
+            f"There are {body['total']} emissions but only {len(results)} fetched, please revise script/query to get all results"
         )
-    return results
-
-
-def import_results_from_file(filename):
-    results = None
-    with open(filename, "r", encoding="utf-8") as jsonfile:
-        results = json.load(jsonfile)
     return results
 
 
 def create_emission_factors_file(results):
     factors = {}
-    types_with_zero_fe = {}
     duplicate_fe_by_unit = {}
     duplicate_count = 0
 
     for emission in results:
         name = emission["Nom_base_français"]
+        unit = emission["Unité_français"]
         if name not in factors:
             factors[name] = {}
-        unit = emission["Unité_français"]
-        # note that there is a duplicate unit for emission type
+
+        # note if there is a duplicate unit for emission type
         if unit in factors[name]:
             if name not in duplicate_fe_by_unit:
                 duplicate_fe_by_unit[name] = {}
@@ -42,18 +35,14 @@ def create_emission_factors_file(results):
             duplicate_count += 1
         emission_factor = emission["Total_poste_non_décomposé"].replace(",", ".")
 
-        # if there is a zero emission factor, note warning. Otherwise, add to factors
-        if emission_factor == "0":
-            types_with_zero_fe[name] = types_with_zero_fe.get(name, 0) + 1
-        elif unit not in factors[name]:  # don't record emission factor of 0
+        # add to factors
+        if unit not in factors[name]:
             factors[name][unit] = emission_factor
 
     with open("auto_emission_factors.json", "w", encoding="utf8") as jsonfile:
         json.dump(factors, jsonfile, indent=2, ensure_ascii=False)
 
     print(duplicate_fe_by_unit)
-    print(types_with_zero_fe)
-    print(f"Total zero factors: {len(types_with_zero_fe.keys())}")
     print(f"Total duplicates: {duplicate_count}")
     return factors
 
