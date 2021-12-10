f5d3fb122e5fa41f03c082f6e7330eb34dda0a1b
Helen Root <helenroot1@gmail.com>
2021-12-10 12:00:01 +0100

    Backoffice : bilan et emission

--- data/admin.py
+++ data/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from data.models import User
+from data.models import User, Report, Emission
 
 
 @admin.register(User)
@@ -46,3 +46,72 @@ class BcsUserAdmin(UserAdmin):
         ),
         (_("Dates importantes"), {"fields": ("last_login", "date_joined")}),
     )
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ("siren", "annee", "raison_sociale", "gestionnaire", "statut", "creation_date", "region", "naf")
+    search_fields = ("raison_sociale", "siren")
+    list_filter = ("annee", "statut", "region", "naf")
+    readonly_fields = (
+        "siren",
+        "annee",
+        "raison_sociale",
+        "gestionnaire",
+        "region",
+        "naf",
+        "statut",
+        "creation_date",
+        "publication_date",
+        "mode",
+        "manuel_poste_1",
+        "manuel_poste_2",
+    )
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "siren",
+                    "annee",
+                    "raison_sociale",
+                    "gestionnaire",
+                    "region",
+                    "naf",
+                    "statut",
+                    "creation_date",
+                    "publication_date",
+                    "mode",
+                    "manuel_poste_1",
+                    "manuel_poste_2",
+                )
+            },
+        ),
+    )
+
+
+@admin.register(Emission)
+class EmissionAdmin(admin.ModelAdmin):
+    list_display = (
+        "type",
+        "bilan",
+        "poste",
+        "valeur",
+        "unite",
+        "localisation",
+        "creation_date",
+    )
+    search_fields = ("type",)
+    readonly_fields = (
+        "bilan",
+        "type",
+        "poste",
+        "valeur",
+        "unite",
+        "creation_date",
+        "localisation",
+        "note",
+    )
+
+    fieldsets = ((None, {"fields": ("type", "poste", "valeur", "unite", "localisation", "note")}),)
