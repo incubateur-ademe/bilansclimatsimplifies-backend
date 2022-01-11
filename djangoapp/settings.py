7c3e99c28bda79d11f83abf90f24ce1ef8702cef
Helen Root <helenroot1@gmail.com>
2022-01-11 19:32:00 +0100

    Missing dependency; update static file collection

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -152,9 +152,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = "staticfiles"
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
